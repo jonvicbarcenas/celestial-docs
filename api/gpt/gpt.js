6afacabf6afe0d010eab94bf3f21d42056ec0aa5
jonvicbarcenas <48144626+jonvicbarcenas@users.noreply.github.com>
2024-01-09 14:48:15 +0000

    1.2.1

--- api/gpt/gpt.js
+++ api/gpt/gpt.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const apiUrl = 'https://chatgayfeyti.archashura.repl.co/';
+const apiUrl = 'https://chatgayfeyti.onrender.com/';
 
 
 router.get('/', async (req, res) => {
