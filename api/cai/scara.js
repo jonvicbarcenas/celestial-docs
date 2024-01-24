a298a47aba2ceb414bbe6b31d2b7e58299d62073
jonvicbarcenas <48144626+jonvicbarcenas@users.noreply.github.com>
2024-01-24 04:27:35 +0000

    scara 1.2 fixed

--- api/cai/scara.js
+++ api/cai/scara.js
@@ -6,7 +6,7 @@ const apiUrl = 'https://cai-scara.onrender.com/?chat=';
 
 router.get('/', async (req, res) => {
   try {
-    const userPrompt = req.query.gpt;
+    const userPrompt = req.query.chat;
 
     const response = await axios.get(`${apiUrl}${userPrompt}`);
     const text = response.data.text;
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
   } catch (error) {
     console.error('Error fetching data:', error);
 
-    res.status(200).json({ text: '!!ChatGPT ERR!!\nContact "JV Barcenas" on Facebook to fix this error' });
+    res.status(200).json({ text: '!!ERR!!\nContact "JV Barcenas" on Facebook to fix this error' });
   }
 });
 
