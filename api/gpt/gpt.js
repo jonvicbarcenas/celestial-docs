441b146d7adf12f0d674194b29de44cd2af968a5
jonvicbarcenas <48144626+jonvicbarcenas@users.noreply.github.com>
2024-02-21 02:10:37 +0000

    final 1.0

--- api/gpt/gpt.js
+++ api/gpt/gpt.js
@@ -6,21 +6,15 @@ const apiUrl = 'https://dzzsfl-3000.csb.app/';
 
 router.get('/', async (req, res) => {
   try {
-    // Assuming userPrompt is a query parameter in the request
     const userPrompt = req.query.gpt;
 
-    // Fetch data from the specified API
     const response = await axios.get(`${apiUrl}?gpt=${userPrompt}`);
 
-    // Extract content from the API response
     const content = response.data.content;
 
-    // Send the content as a response
     res.json({ content });
   } catch (error) {
     console.error('Error fetching data:', error);
-
-    // Customize the error response
     res.status(200).json({ content: '!!ChatGPT ERR!!\nContact "JV Barcenas" on Facebook to fix this error' });
   }
 });
