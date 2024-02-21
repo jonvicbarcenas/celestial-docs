7ada39b149c248c2e353f15fd702734d1d89e752
jonvicbarcenas <48144626+jonvicbarcenas@users.noreply.github.com>
2024-02-21 02:09:13 +0000

    fix wrong spelling

--- api/gpt/gpt.js
+++ api/gpt/gpt.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const apiUrl = 'https://dzzsfl-3000.csb.ap/';
+const apiUrl = 'https://dzzsfl-3000.csb.app/';
 
 router.get('/', async (req, res) => {
   try {
